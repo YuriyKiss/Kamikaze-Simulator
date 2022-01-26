e5f9887a5695fd77cf8e190cb65cd207866cfad1
Yuriy Kis <yur.cis3@gmail.com>
2022-01-26 13:33:43 +0200

    logs removed, idle feature added

--- Assets/Scripts/PuppetController.cs
+++ Assets/Scripts/PuppetController.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using System.Collections;
-using System;
 
 using RootMotion.Dynamics;
+using Input = InputWrapper.Input;
 
 public class PuppetController : MonoBehaviour
 {
@@ -10,12 +10,52 @@ public class PuppetController : MonoBehaviour
     private AnimatedCharacterController charAnim;
 
     private PuppetMaster puppet;
+    private Animator anim;
+
+    private float timer = 0f;
+    private bool targetStanding = false;
+    private string danceAnimation = "Dancing";
+    private string idleAnimation = "Standing";
 
     private float puppetRessurectionTime = 0.3f;
 
     private void Start()
     {
         puppet = GetComponentInParent<PuppetMaster>();
+        anim = charAnim.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt("InGame") == 1)
+        {
+            if (Input.TouchCount > 0)
+            {
+                timer = 0f;
+            }
+
+            if (Physics.Raycast(transform.position, Vector3.down, 0.5f, 1 << 0))
+            {
+                timer += Time.deltaTime;
+            }
+
+            if (timer > 1.5f && !targetStanding)
+            {
+                targetStanding = true;
+
+                charAnim.SetPosition(transform);
+
+                puppet.state = PuppetMaster.State.Alive;
+
+                anim.Play(idleAnimation);
+            }
+            else if (timer <= 1.5f)
+            {
+                targetStanding = false;
+
+                puppet.state = PuppetMaster.State.Dead;
+            }
+        }
     }
 
     public IEnumerator SetPuppetAlive()
@@ -28,5 +68,7 @@ public class PuppetController : MonoBehaviour
         charAnim.SetPosition(transform);
 
         puppet.state = PuppetMaster.State.Alive;
+
+        anim.Play(danceAnimation);
     }
 }
